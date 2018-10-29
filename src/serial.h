e956f9d2766e6e5e6bbe09e1dbf1310a12cfa309
Alekseev Artem <a.artem060@gmail.com>
2018-10-29 23:09:57 +0300

    Update src/serial.h
    Co-Authored-By: AlisherGM <alisher.gulov97@mail.ru>

--- src/serial.h
+++ src/serial.h
@@ -4,7 +4,7 @@
 
 #define SERIAL_COM1                0x3F8   
 #define SERIAL_COM2                0x2F8
-#define SERIAL_COM3_BASE                0x3E8    
+#define SERIAL_COM3                0x3E8    
 #define SERIAL_COM4_BASE                0x2E8    
 
 /*!
