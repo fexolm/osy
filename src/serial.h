b9d7845be7fde808f05827e2ab37ab30d81f8096
Alekseev Artem <a.artem060@gmail.com>
2018-10-29 23:09:52 +0300

    Update src/serial.h
    Co-Authored-By: AlisherGM <alisher.gulov97@mail.ru>

--- src/serial.h
+++ src/serial.h
@@ -2,7 +2,7 @@
 #define SERIAL_H
 #include "stdint.h"
 
-#define SERIAL_COM1_BASE                0x3F8   
+#define SERIAL_COM1                0x3F8   
 #define SERIAL_COM2_BASE                0x2F8
 #define SERIAL_COM3_BASE                0x3E8    
 #define SERIAL_COM4_BASE                0x2E8    
